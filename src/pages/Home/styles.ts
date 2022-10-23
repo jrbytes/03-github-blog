dc8bbca87dd946542fd66707a5818ec1f98e41a9
Jr Bytes <itamazor@gmail.com>
2022-10-23 14:24:18 -0300

    updated home page, responsive css

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -15,5 +15,13 @@ export const Posts = styled.div`
         display: none;
       }
     }
+
+    @media (max-width: ${theme.content.width}) {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      padding: 0 ${theme.spacings.small};
+      gap: ${theme.spacings.small};
+    }
   `}
 `
